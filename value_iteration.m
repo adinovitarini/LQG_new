14629f4aeabdd0a337859f82b7674adc9bf61837
Adi Novitarini <adinovitarini@gmail.com>
2022-07-26 16:11:57 +0700

    Update value_iteration.m

--- value_iteration.m
+++ value_iteration.m
@@ -1,4 +1,4 @@
-function [P,K,G] = value_iteration(Tbar,N,Bonebar,Q,R,discount_fac)
+function [P,K,G] = value_iteration(Tbar,N,Bonebar,Q,R)
 %Value Iteration Algorithm
 % K = rand(1,size(T,1));
 P = zeros(size(Tbar,1),size(Tbar,1));
@@ -6,7 +6,7 @@ K = zeros(1,size(Tbar,1));
 [Pdare,Kdare] = idare(Tbar,Bonebar,Q,R);
 for j = 1:N
     if j~=1
-    P_new = Q + K'*R*K+discount_fac*((Tbar-Bonebar*K)'*P*(Tbar-Bonebar*K));
+    P_new = Q + K'*R*K+((Tbar-Bonebar*K)'*P*(Tbar-Bonebar*K));
     K_new = inv(R+Bonebar'*P_new*Bonebar)*Bonebar'*P_new*Tbar;
     P = P_new;
     K = K_new;
